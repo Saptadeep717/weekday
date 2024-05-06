acae6a9e8decd536b7f2a51613931bc155f74b5b
Saptadeep <saptadeepsenab717@gmail.com>
2024-05-06 22:14:17 +0530

    fourth commit -> infinite scroll handler update

--- src/App.js
+++ src/App.js
@@ -1,53 +1,61 @@
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import { useDispatch } from "react-redux";
-import Jobs from "./components/Jobs";
+import React, { useState, useEffect } from 'react';
+import "./App.css"
+import { useDispatch } from 'react-redux';
+import Jobs from './components/Jobs';
 import { addAllJobs } from "./redux/jobSlice";
 
 function App() {
-  const [offset, setOffset] = useState(0);
-  const [loading, setLoading] = useState(false);
+  const [offset, setOffset] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const [fitItem]=useState("")
 
   const dispatch = useDispatch();
 
   const FetchPosts = async () => {
-    setLoading(true);
+    setLoading(true)
     const myHeaders = new Headers();
     myHeaders.append("Content-Type", "application/json");
 
     const body = JSON.stringify({
-      limit: 10,
-      offset: offset,
+      "limit": 10,
+      "offset": offset
     });
 
     const requestOptions = {
       method: "POST",
       headers: myHeaders,
-      body,
+      body
     };
 
-    fetch(
-      "https://api.weekday.technology/adhoc/getSampleJdJSON",
-      requestOptions
-    )
+    fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions)
       .then((response) => response.json())
       .then((result) => {
-        dispatch(addAllJobs(result.jdList));
-        setLoading(false);
+        dispatch(addAllJobs(result.jdList))
+        setLoading(false)
       })
       .catch((error) => {
-        console.error(error);
-        setLoading(false);
+        console.error(error)
+        setLoading(false)
       });
-  };
+
+  }
   useEffect(() => {
     FetchPosts();
-  }, [offset]);
+  }, [offset])
 
+ 
+
+  const handleScroll = (e) => {
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+    if (scrollTop + clientHeight >= scrollHeight - 100 && !loading) {
+      setOffset(offset + 10)
+     
+    }
+  }
   return (
-    <div>
+    <div style={{ height: "800px", overflowY: "scroll" }} onScroll={handleScroll}>
       <h1>Candidate Application Platform</h1>
-      <Jobs />
+      <Jobs/>
     </div>
   );
 }
